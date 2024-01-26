05c75785846a1a5c9d96b0abf8bc6fc2f9592aba
Aapo Rissanen <aapo.rissanen@myy.haaga-helia.fi>
2024-01-26 13:18:04 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import * as Speech from "expo-speech";
+import { useState } from "react";
 
 export default function App() {
+  const [text, setText] = useState();
+
+  const speak = () => {
+    Speech.speak(text);
+  };
+
+  const handleInputChange = (text) => {
+    setText(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{
+          width: "80%",
+          height: 40,
+          borderWidth: 2,
+          borderColor: "black",
+        }}
+        value={text}
+        onChangeText={(text) => handleInputChange(text)}
+      />
+      <Pressable
+        onPress={speak}
+        style={{
+          padding: 20,
+          margin: 20,
+          backgroundColor: "blue",
+        }}
+      >
+        <Text style={{ color: "white" }}>Press to hear the text</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
